d7e39995c7c4163870817ea7d06c28894f44b1e4
Andrew Campbell <andycampbell92@gmail.com>
2016-03-19 21:01:48 +0000

    got tweet builder working

--- follow-mp/lib/tasks/vote_tweeter.rb
+++ follow-mp/lib/tasks/vote_tweeter.rb
@@ -1,12 +1,21 @@
 module MpVoteTweeter
     class VoteTweeter
+        @@base_url = 'http://www.publicwhip.org.uk/'
         def tweet_votes(votes)
             tweets = votes.map {|vote| build_tweet(vote)}
-            puts tweets
+        end
+
+        def truncate_title(tweet_content, title)
+            # 140 - 25 - 1 - 1 = 113 Assume 25 chars for link, -1 because we start at 0 index and -1 because of space
+            title[0, 113 - tweet_content.length]
         end
 
         def build_tweet(vote)
-            vote.title
+            simple_vote_text = vote.simple_vote_text
+            vote_action = (simple_vote_text.include? "absent") ? "was absent" : "voted #{simple_vote_text}"
+            tweet_content = "#{vote.mp.name} #{vote_action} on "
+            short_title = truncate_title(tweet_content, vote.title)
+            tweet = "#{tweet_content}#{short_title} #{@@base_url}#{vote.link}"
         end
 
         def send_tweets(tweets)
